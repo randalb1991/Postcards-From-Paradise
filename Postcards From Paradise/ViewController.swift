8ec3bdb3ae14bb1ff6c8b166ba9b80cdef122598
Daniel Barrientos <rd.barrientosa@gmail.com>
2017-10-05 20:59:44 +0200

    Initial Commit

---
+++ Postcards From Paradise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcards From Paradise
+//
+//  Created by Randall Dani Barrientos Alva on 5/10/17.
+//  Copyright © 2017 Randall Dani Barrientos Alva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
